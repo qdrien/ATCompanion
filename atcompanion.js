abe790a8db140029e2827fc4b214a4099b0f5854
Adrien Coppens <qdrien@gmail.com>
2017-06-20 14:24:17 +0200

    +copy pw button now only appears when the key is ready to be copied +the bg script now only sends the key once (and on-demand) +sliding panel and its button +jQuery to ease future developments and improve readability +refactorings

--- atcompanion.js
+++ atcompanion.js
@@ -1,14 +1,5 @@
 const SCRIPT_TAG = "[ATC-c]\n";
 
-//Injecting a script in the page itself
-/*var s = document.createElement('script');
-s.src = browser.extension.getURL('monkeyRequests.js');
-s.onload = function (){
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(s);*/
-
-
 document.getElementById('floating_game_holder').scrollIntoView(); //scroll the page to get to the game zone
 const chatWidth = document.getElementById('chat_container_cell').offsetWidth;
 const totalWidth = document.body.offsetWidth;
@@ -18,37 +9,103 @@ document.body.style.height = '100%';
 document.documentElement.style.width = '100%';
 document.body.style.width = '100%';
 
-const div = document.createElement('div');
-document.body.appendChild(div);
-div.id = 'atcDiv';
-div.style.position = 'fixed';
-div.style.top = '0';
-div.style.left = 100 * (totalWidth - chatWidth) / totalWidth + '%'; //basically on top of the chat
-div.style.width = (100 * chatWidth / totalWidth) + '%';
-div.style.height = '100%';
-div.style.backgroundColor = 'rgb(129,2,66)';
-div.style.textAlign = 'center';
+//a panel that will contain all add-on elements
+const container = $("<div id='atcDiv'></div>");
+const containerDefaultLeft = totalWidth - chatWidth;
+$(container).css({
+    "position": "fixed",
+    "top": 0,
+    "left": containerDefaultLeft, //basically on top of the chat
+    "width": chatWidth,
+    "height": "100%",
+    "background-color": "rgb(129,2,66)",
+    "text-align": "center",
+});
+$("body").append(container);
 
-const pwContent = document.createElement('input');
-pwContent.id = 'pwkey';
-pwContent.setAttribute("type", "hidden");
-div.appendChild(pwContent);
-
-const btn = document.createElement('button'); //TODO: should probably only appear when the password has been extracted
-btn.type = 'button';
-btn.innerHTML = 'Copy password key';
-div.appendChild(btn);
-
-
-btn.addEventListener("click", () => {
-    const copyText = document.querySelector("#pwkey");
-    copyText.removeAttribute("type"); //apparently necessary bs as you can only copy DOM elements to the clipboard
-    copyText.select();
-    document.execCommand("copy");
-    pwContent.setAttribute("type", "hidden");
+//a slider button to toggle the container panel
+const slider = $("<img src="+ browser.extension.getURL("img/slide-right.png") +" />");
+$(slider).css({
+    "position": "fixed",
+    "top": 0,
+    "bottom": 0,
+    "margin": "auto",
+    "left": containerDefaultLeft - 20,
+    "width": 80
+});
+$("body").append(slider);
+//slider interaction
+$(slider).click(function (){
+    const currentLeft = $(container).css("left");
+    console.log(SCRIPT_TAG + parseInt(currentLeft) + " // " + containerDefaultLeft);
+    if(parseInt(currentLeft) <= containerDefaultLeft){
+        $(container).animate({
+            left: '+='+ (chatWidth - 10)
+        });
+        $(slider).animate({
+            left: '+='+ (chatWidth - 50)
+        });
+        $(slider).attr("src", browser.extension.getURL("img/slide-left.png"));
+    }
+    else {
+        $(container).animate({
+            left: '-='+ (chatWidth - 10)
+        });
+        $(slider).animate({
+            left: '-='+ (chatWidth - 50)
+        });
+        $(slider).attr("src", browser.extension.getURL("img/slide-right.png"));
+    }
 });
 
+//the hidden input element that will contain the password key
+const pwContent = $("<input id='pwkey'/>");
+$(pwContent).hide();
+$(container).append(pwContent);
+
+//a button to copy the key to the clipboard
+const btn = $("<button type='button'>Copy password key</button>");
+$(container).append(btn);
+$(btn).hide();
+
+$(btn).click(function(){
+    $(pwContent).show();
+    $(pwContent)[0].select();
+    document.execCommand("copy");
+    $(pwContent).hide();
+});
+
+//a loading image that is displayed until the password key is extracted
+const loadingGif = $("<img src="+ browser.extension.getURL("img/loading.gif") +" />");
+$(loadingGif).css({
+    "width":"100%",
+    "position": "absolute",
+    "top": 0,
+    "bottom": 0,
+    "margin": "auto",
+    "left": 0
+});
+$(container).append(loadingGif);
+
+//Messages handlers to communicate with the background script (runtime API)
 browser.runtime.onMessage.addListener(request => {
-    pwContent.value = request.password;
+    handleResponse(request);
     return Promise.resolve({response: ""});
 });
+
+const sending = browser.runtime.sendMessage({
+    type: "pwrequest"
+});
+sending.then(handleResponse, handleError);
+
+function handleResponse(response) {
+    if(response.password !== ""){
+        $(pwContent).val(response.password);
+        $(btn).show();
+        $(loadingGif).remove();
+    }
+}
+
+function handleError(error) {
+  console.log(`Error: ${error}`);
+}
--- background.js
+++ background.js
@@ -1,13 +1,21 @@
 const SCRIPT_TAG = "[ATC-bg]\n";
 let password = "";
 
+//Requests handlers to react to API calls (webRequest API)
+//This will hopefully be extended with other features
+//when a read-access to response bodies will be available (without any hacks)
+browser.webRequest.onBeforeRequest.addListener(
+    handleAPICall,
+    {urls: ["*://*.cb-live.synapse-games.com/api.php*"]},
+    ['requestBody']
+);
+
 function handleAPICall(requestDetails) {
     console.log(SCRIPT_TAG + requestDetails.url);
     if (requestDetails.method === "POST") {
-        let newPassword = requestDetails.requestBody.formData.password;
-        if (newPassword !== password) { //TODO: should probably only send it once as it won't change
-            password = newPassword;
-
+        const pw = requestDetails.requestBody.formData.password;
+        if (password.length <= 0 && !(!pw || 0 === pw.length)) {
+            password = pw;
             browser.tabs.query({
                 currentWindow: true,
                 active: true
@@ -16,11 +24,13 @@ function handleAPICall(requestDetails) {
     }
 }
 
-browser.webRequest.onBeforeRequest.addListener(
-    handleAPICall,
-    {urls: ["*://*.cb-live.synapse-games.com/api.php*"]},
-    ['requestBody']
-);
+function onError(error) {
+    console.error(`${SCRIPT_TAG} Error: ${error}`);
+}
+
+
+//Messages handlers to communicate with the main (component) script (runtime API)
+browser.runtime.onMessage.addListener(handleMessage);
 
 function sendPassword(tabs) {
     for (let tab of tabs) {
@@ -31,6 +41,8 @@ function sendPassword(tabs) {
     }
 }
 
-function onError(error) {
-    console.error(`${SCRIPT_TAG} Error: ${error}`);
+function handleMessage(request, sender, sendResponse) {
+    if (request.type === "pwrequest") {
+        sendResponse({password: password});
+    }
 }
